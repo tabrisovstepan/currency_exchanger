5006a890b5be108a759c59e3e8abe4940ae2dc67
Tabrisov Stepan <tabrisovstepan@gmail.com>
2024-11-16 21:14:33 +0300

    Added lombok, jackson dependencies. Create entities.

--- src/main/java/org/example/Entities/Currency.java
+++ src/main/java/org/example/Entities/Currency.java
@@ -1,4 +1,22 @@
 package org.example.Entities;
 
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class Currency {
+    private Long id;
+
+    @NonNull
+    private String code;
+
+    @NonNull
+    private String fullName;
+
+    @NonNull
+    private String sign;
 }
--- src/main/java/org/example/Entities/ExchangeRate.java
+++ src/main/java/org/example/Entities/ExchangeRate.java
@@ -1,4 +1,19 @@
 package org.example.Entities;
 
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class ExchangeRate {
+    private Long id;
+    private Long baseCurrencyId;
+    private Long targetCurrencyId;
+
+    @NonNull
+    private BigDecimal rate;
 }
